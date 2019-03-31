f0c652e3916901e7904a1ef5c678d9105c836c2f
sandeshgiri <sandesh.giri@gmail.com>
2019-03-31 12:58:16 +0530

    Added LargestOfThreeNumbers.java-TBC

---
+++ JavaExamples/src/JavaPrograms/LargestOfThreeNumbers.java
@@ -0,0 +1,26 @@
+package JavaPrograms;
+
+import java.util.Scanner;
+
+public class LargestOfThreeNumbers {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the 3 numbers");
+		int a[]=new int[3];
+		Scanner sc=new Scanner(System.in);
+		
+		for(int i=0;i<3;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		
+		System.out.println("Entered numbers are:");
+		for(int j=0;j<3;j++)
+		{
+			System.out.println(a[j]);
+		}
+
+	}
+
+}
