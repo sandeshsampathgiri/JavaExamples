bd0cea1cd5db698bcc7bb22ce0d18972a9868f18
sandeshgiri <sandesh.giri@gmail.com>
2019-03-31 09:51:56 +0530

    Added PrimeOrNot.java

---
+++ JavaExamples/src/JavaPrograms/PrimeOrNot.java
@@ -0,0 +1,30 @@
+package JavaPrograms;
+
+import java.util.Scanner;
+
+public class PrimeOrNot {
+
+	public static void main(String[] args) {
+
+		int flag = 0;
+		System.out.println("Enter the number:");
+
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+
+		for (int i = 2; i <= (num / 2); i++) {
+			if (num % i == 0) {
+				flag = 1;
+				break;
+			}
+		}
+
+		if (flag == 0) {
+			System.out.println("Entered number is prime");
+		} else {
+			System.out.println("Entered number is not prime");
+		}
+
+	}
+
+}
