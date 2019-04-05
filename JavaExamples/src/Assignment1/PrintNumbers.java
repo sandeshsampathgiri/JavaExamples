faa66ebb85fb3c76c2ea151afd94c3b9361692f4
sandeshgiri <sandesh.giri@gmail.com>
2019-04-05 19:53:32 +0530

    Updated PrintNumbers.java

--- JavaExamples/src/Assignment1/PrintNumbers.java
+++ JavaExamples/src/Assignment1/PrintNumbers.java
@@ -3,12 +3,10 @@ package Assignment1;
 public class PrintNumbers {
 
 	public static void main(String[] args) {
-	
-		for(int i=0;i<4;i++)
-		{
-			for(int j=0;j<=9;j++)
-			{
-				System.out.print(i+""+j+"\t");
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j <= 9; j++) {
+				System.out.print(i + "" + j + "\t");
 			}
 			System.out.println();
 		}
