a04ff9bed504c950b0a3cd18950d8c22a841aaa6
sandeshgiri <sandesh.giri@gmail.com>
2019-04-05 19:27:02 +0530

    Added PrintValueOfN.java

---
+++ JavaExamples/src/Assignment1/PrintValueOfN.java
@@ -0,0 +1,22 @@
+package Assignment1;
+
+/*
+ * Question 3: Try to print the following pattern on the console:
+n = 4
+n = 3
+n = 2
+n = 1
+n = 0
+*/
+
+public class PrintValueOfN {
+
+	public static void main(String[] args) {
+
+		for (int i = 4; i >= 0; i--) {
+			System.out.println("n = " + i);
+		}
+
+	}
+
+}
