557aeb2e1604002bb98552b5e8fd82c87e721a36
sandeshgiri <sandesh.giri@gmail.com>
2019-04-05 19:51:07 +0530

    Added PrintNumbers.java

---
+++ JavaExamples/src/Assignment1/PrintNumbers.java
@@ -0,0 +1,18 @@
+package Assignment1;
+
+public class PrintNumbers {
+
+	public static void main(String[] args) {
+	
+		for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<=9;j++)
+			{
+				System.out.print(i+""+j+"\t");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
